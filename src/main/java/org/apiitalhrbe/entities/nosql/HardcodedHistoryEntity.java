b45b1e63d61d5da93b4d0dcb30e83e01e38e5411
Imanol Suppo <128556432+114184-SUPPO-IMANOL@users.noreply.github.com>
2024-08-07 23:43:24 -0300

    Add files via upload

---
+++ src/main/java/org/apiitalhrbe/entities/nosql/HardcodedHistoryEntity.java
@@ -0,0 +1,27 @@
+package org.apiitalhrbe.entities.nosql;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.UUID;
+
+@Document(collection = "hardcoded_history")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HardcodedHistoryEntity implements HistoryEntity {
+
+    @Id
+    private UUID id;
+
+    private LocalDate from;
+
+    private LocalDate to;
+
+    private Map<String, Integer> hardcodedValues;
+}
